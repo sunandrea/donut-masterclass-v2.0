7e5ae7ccfc1039fbdd218dc3a663176f4a5f4bbb
SunAndrea <andriishtohryn@gmail.com>
2022-11-21 09:59:37 +0200

    burger nav anchor

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLinks = document.querySelectorAll('.burger-nav__link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +18,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  menuLinks.forEach(link => link.addEventListener('click', toggleMenu));
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
